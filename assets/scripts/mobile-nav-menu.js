47076620eea07f38ed400d512a3538138fc3c6b0
Allison Tarr <hello@allisontarr.com>
2017-08-25 15:06:57 -0400

    change mobile nav to slide-in version

--- assets/scripts/mobile-nav-menu.js
+++ assets/scripts/mobile-nav-menu.js
@@ -1,12 +1,13 @@
-/**
- * file mobile-nav-menu.js
- *
- * Mobile Navigation Menu
- */
-window.wdsMobileNav = {};
-( function ( window, $, app ) {
-	// Constructor
-	app.init = function () {
+ /**
+  * File mobile-nav-menu.js
+  *
+  * Based on sliding panel from http://refills.bourbon.io/components/
+  */
+window.Mobile_Nav_Slide = {};
+( function( window, $, app ) {
+
+	// Constructor.
+	app.init = function() {
 		app.cache();
 
 		if ( app.meetsRequirements() ) {
@@ -14,103 +15,36 @@ window.wdsMobileNav = {};
 		}
 	};
 
-	// Cache all the things
-	app.cache = function () {
+	// Cache all the things.
+	app.cache = function() {
 		app.$c = {
-			'window': $( window ),
-			'body': $( 'body' ),
-			'mobileNavMenuContainer': $( '.mobile-nav-menu' ),
-			'menuItemCount': $( '.mobile-nav-menu .mobile-nav > li' ).length
+			window: $(window),
+			body: $( 'body' ),
+			button: $( '.sliding-panel-button' ),
+			nav: $( '.menu-primary-menu-container' ),
+			panelContent: $( '.sliding-panel-content' ),
 		};
 	};
 
-	// Combine all events
-	app.bindEvents = function () {
-		// Do nothing if there are not more than 5 links
-		if ( app.$c.menuItemCount <= 5 ) {
-			return;
-		}
-
-		// Replace the fifth menu item with a "more" link
-		app.replaceLastMenuItem();
-
-		// Show more items when the "more" item is clicked
-		app.$c.body.on( 'click', '.mobile-menu-more-link', app.displayMoreItems );
-
-		// Add the more classes when hovering a parent menu item
-		app.$c.body.on( 'click', '.mobile-nav-menu .menu-item-has-children > a', app.setSecondClick );
-
-		// Hide the menu when the close button is clicked
-		app.$c.body.on( 'click', '.close-mobile-menu', app.hideMoreItems );
+	// Combine all events.
+	app.bindEvents = function() {
+		app.$c.button.on( 'click', app.toggleNav );
 	};
 
 	// Do we meet the requirements?
-	app.meetsRequirements = function () {
-		return app.$c.mobileNavMenuContainer.length;
+	app.meetsRequirements = function() {
+		return app.$c.panelContent.length;
 	};
 
-	// Replace the fifth menu item with a "more" link
-	app.replaceLastMenuItem = function () {
-		// By this point, we know we have at least 5 items
-		// Add our "more" link
-		$( '.mobile-nav-menu .mobile-nav > li:nth-child(4)' ).after( '<li class="mobile-menu-more-link"><a href="#"><span><i class="more-icon"></i>More</span></a></li>' );
-
-		var boundary = $( '.mobile-nav-menu .mobile-nav > li:nth-child(6)' );
-
-		$( '<ul class="menu mobile-nav-menu-hidden">' ).insertAfter( boundary.parent() ).append( boundary.nextAll().andSelf() );
+	// Toggle the form open and close.
+	app.toggleNav = function(e) {
+		app.$c.body.toggleClass( 'sidebar-is-open' );
+		app.$c.button.toggleClass( 'open' );
+		app.$c.panelContent.toggleClass( 'is-visible' );
+		app.$c.nav.toggleClass( 'is-visible' );
 	};
 
-	// Toggle the menu items on a click of the "more" link
-	app.displayMoreItems = function ( event ) {
-		event.preventDefault();
-
-		// Hide the menu if it's already opened
-		if ( app.$c.body.hasClass( 'mobile-menu-more' ) && !app.$c.body.hasClass( 'sub-menu-more' ) ) {
-			app.removeMenuClasses();
-			return;
-		}
-
-		// Remove any instances of classes already in place
-		// This makes sure we can click to switch between submenus
-		app.removeMenuClasses();
-
-		app.$c.mobileNavMenuContainer.toggleClass( 'more' );
-		app.$c.body.toggleClass( 'mobile-menu-more' );
-	};
-
-	// Let the submenu parent be a normal link on the second click
-	app.setSecondClick = function ( event ) {
-		// Check to see if this parent has the visible class
-		if( !$( this ).parent( 'li' ).hasClass( 'visible' ) ) {
-			// Don't let the link fire as a normal link
-			event.preventDefault();
-		}
-
-		// Remove any instances of classes already in place
-		// This makes sure we can click to switch between submenus
-		app.removeMenuClasses();
-
-		// Toggle the class to display the submenu
-		$( this ).parent( 'li' ).toggleClass( 'visible' );
-
-		// Add our "more" classes as we do when clicking the "More" link
-		app.$c.mobileNavMenuContainer.toggleClass( 'more' );
-		app.$c.body.toggleClass( 'mobile-menu-more sub-menu-more' );
-	};
-
-	// Hide the menu items
-	app.hideMoreItems = function () {
-		app.removeMenuClasses();
-	};
-
-	app.removeMenuClasses = function () {
-		// Remove any instances of classes already in place
-		// This makes sure we can click to switch between submenus
-		app.$c.body.removeClass( 'mobile-menu-more sub-menu-more' );
-		app.$c.mobileNavMenuContainer.removeClass( 'more' );
-		$( '.menu-item-has-children' ).removeClass( 'visible' );
-	};
-
-	// Engage
+	// Engage!
 	$( app.init );
-} )( window, jQuery, window.wdsMobileNav );
+
+})( window, jQuery, window.Mobile_Nav_Slide );
